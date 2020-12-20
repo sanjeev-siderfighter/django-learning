ee745307eb8ebc55fbb1c9083a83948521a9c73b
Sanjeev Kumar <sanjeev.kumar@box8.in>
2020-12-20 17:18:52 +0530

    Update with help from videos

--- Django_Learning/ecomsite/shop/views.py
+++ Django_Learning/ecomsite/shop/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from math import ceil
+from .models import Product
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n // 4 + ceil((n / 4) - (n // 4))
+
+    params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'products': products}
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
-    return HttpResponse("about page")
+    return render(request, 'shop/about.html')
 
 
 def contact(request):
@@ -21,7 +30,10 @@ def tracker(request):
 
 
 def search(request):
-    return HttpResponse("search page")
+    prod_list = Product.objects.all()
+    print(type(prod_list))
+    return HttpResponse(prod_list)
+    # return HttpResponse("search page")
 
 
 def productView(request):
